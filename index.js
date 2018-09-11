c5cb64fe5b91b264b0e10fd646ad6440472020d0
姚诚 <yaocheng@lattebank.com>
2018-09-11 10:23:06 +0800

    first of gitbook rebuild

--- index.js
+++ index.js
@@ -7,20 +7,4 @@ module.exports = {
             'buttons.js'
         ]
     },
-    hooks: {
-        // Compatibility layer for gitbook < 2.5.0
-        config: function(cfg) {
-            var sharingLink = _.get(cfg, 'links.sharing', {});
-            cfg.pluginsConfig.sharing = _.defaults(cfg.pluginsConfig.sharing || {}, {});
-
-            _.each(sharingLink, function(enabled, type) {
-                if (enabled != false) return;
-
-                if (type == 'all') cfg.pluginsConfig.sharing[type] = [];
-                else cfg.pluginsConfig.sharing[type] = false;
-            });
-
-            return cfg;
-        }
-    }
 };
